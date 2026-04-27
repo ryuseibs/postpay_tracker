d61636cbe30bfdec5c564d0ba1434d2fccbc5a00
ryuseibs <beryusei0923212ssyo@aol.com>
2026-04-27 23:29:56 +0900

    通知の中身作成（コミットミスのため、再度対象にコミット）

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,16 @@
-import UIKit
+import UserNotifications
 
-var greeting = "Hello, playground"
+// 通知の中身を用意する
+let content = UNMutableNotificationContent()
+
+// 通知タイトル・通知内容
+content.title = "テスト通知"
+content.body = "これは通知テストです。"
+
+// 通知アクションのカテゴリ登録
+content.categoryIdentifier = "MY_REPLY_CATEGORY"
+
+// 確認
+print(content.title)
+
+
